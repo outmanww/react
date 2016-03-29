85717a80b7c34dc09266f6909e9b79150ef4c28c
shiichi <s.shiichi311041@gmail.com>
2016-03-29 15:55:22 +0900

    modify student controller

--- app/Http/Controllers/Student/RoomController.php
+++ app/Http/Controllers/Student/RoomController.php
@@ -17,7 +17,14 @@ class RoomController extends Controller
     public function room($room_id)
     {
     	if ($room_id < 6) {
-    		return '線形代数１&山田太郎&火曜' . $room_id . '限';
+    		// return '線形代数１&山田太郎&火曜' . $room_id . '限';
+            $res = [
+                'name' => '線形代数１',
+                'teacher' => '山田太郎',
+                'lectured_at' => '火曜' . $room_id . '限',
+            ];
+
+            return \Response::json($res, 400);
     	}
         return \Response::json('not found', 400);
     }
