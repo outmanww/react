8489c137f63085ec741e03b8e53d2a01538f1123
shiichi <s.shiichi311041@gmail.com>
2016-04-04 16:50:25 +0900

    edit reaction api

--- app/Models/Lecture/Room.php
+++ app/Models/Lecture/Room.php
@@ -5,6 +5,8 @@ namespace App\Models\Lecture;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+// Models
+use App\Models\Student\Reaction;
 
 class Room extends Model
 {
@@ -28,5 +30,37 @@ class Room extends Model
 	public function lecture()
 	{
 		return $this->belongsTo('App\Models\Lecture\Lecture');
+	}
+
+	public function statusPie($affiliation_id, $interval, $type_id)
+	{
+		$results = array(
+            'total' => 0,
+            'react' => 0,
+            );
+		/*
+		$results['total'] = Reaction::where('affiliation_id', $affiliation_id)
+			->where('room_id', $this->id)
+			->where('action_id', config('controller.action.basic'))
+			->groupBy('student_id')
+			->havingRow('COUNT(CASE type_id WHEN '.config('controller.b_type.room_in').' THEN 1 ELSE NULL END) > COUNT(CASE type_id WHEN '.config('controller.b_type.room_out.'' THEN 1 ELSE NULL END)')
+			->get()
+			->count();
+
+		$results['react'] = Reaction::where('affiliation_id', $affiliation_id)
+			->where('room_id', $this->id)
+	        ->whereIn('action_id', [config('controller.action.reaction_anonymous'),config('controller.action.reaction_realname')])
+            ->where('created_at', '>', Carbon::now()->subMinutes($interval))
+			->where('type_id', $type_id)
+			->groupBy('student_id')
+			->havingRow('COUNT(CASE type_id WHEN '.config('controller.b_type.room_in').' THEN 1 ELSE NULL END) > COUNT(CASE type_id WHEN '.config('controller.b_type.room_out.'' THEN 1 ELSE NULL END)')
+			->get()
+			->count();
+*/
+		return $results;
+	}
+	public function statusLine()
+	{
+		
 	}
 }
