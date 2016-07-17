030bf5fdb04c2f6b9c48bd9636708fe319ced962
shiichi <s.shiichi311041@gmail.com>
2016-07-17 21:48:29 +0900

    add shikoku-kakusan demo

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\EnableCors::class,
     ];
 
     /**
